698177015a1ab9151cbb7531dc5a82564bc9cf8f
sanyam <sanyamjainhbtu@gmail.com>
2025-02-18 16:07:47 +0530

    Edit Delete Route

--- Internship/YardStick/my-app/app/api/transactions/route.ts
+++ Internship/YardStick/my-app/app/api/transactions/route.ts
@@ -31,3 +31,30 @@ export async function GET(){
         return NextResponse.json({ error: "Failed to fetch transactions" }, { status: 500 });
     }
 }
+
+export async function DELETE(req : Request){
+    const {id} = await req.json();
+    try {
+        await connect();
+        if (!id) {
+            return NextResponse.json({ error: "Transaction ID is required" }, { status: 400 });
+        }
+
+        const deletedTransaction = await Transaction.findByIdAndDelete(id);
+        if (!deletedTransaction) {
+            return NextResponse.json({ error: "Transaction not found" }, { status: 404 });
+        }
+
+        return NextResponse.json(
+            {
+                success: true,
+                message: 'Transaction deleted successfully',
+                data: deletedTransaction,
+            },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json({ error: "Transaction deletion failed" }, { status: 500 });
+ 
+    }
+}
--- Internship/YardStick/my-app/lib/getDeleteTransaction.ts
+++ Internship/YardStick/my-app/lib/getDeleteTransaction.ts
@@ -2,8 +2,11 @@ import axios from "axios";
 
 export async function getDeleteTransaction(id : string) {
     try {
-        const response = await axios.delete(`http://localhost:3000/api/transactions/${id}`);
-        console.log(response.data);
+        const response = await axios.delete("http://localhost:3000/api/transactions",{
+           data : {
+            id : id
+           }
+        });
         return response.data;
         
     } catch (error) {
